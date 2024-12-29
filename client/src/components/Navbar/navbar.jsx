7ecbb344268cad46eb5bb9f82c092f44f2b41ce4
Phobial <harshalpatilcsn2020@gmail.com>
2024-12-30 00:58:47 +0530

    new nav bar and carousel added

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,30 +1,31 @@
 
+"use client";
+
 import { Button, Navbar } from "flowbite-react";
-import Logo from '../../Assets/navbar/logo.png'
+import logo from '../../Assets/navbar/logo.png'
 import logout from '../Navbar/logout.png'
 
 export function Nav() {
   return (
-    <Navbar   className="  pt-10 h-[80px] bg-black">
-      <Navbar.Brand href="/" className="mt-[10px]">
-        <img src={Logo} className="mr-3 h-8 md: h-11 " alt="GoLab Logo" />
-        {/* <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white"></span> */}
+    <Navbar fluid rounded className="bg-cyan-50">
+      <Navbar.Brand href="/">
+        <img src={logo} className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
       </Navbar.Brand>
+
       <Navbar.Toggle />
 
-     
       <Navbar.Collapse className="mt-[10px]">
-        <Navbar.Link href="/"  className="text-xl text-white">Home</Navbar.Link>
-        <Navbar.Link href="#" className="text-xl text-white ">About</Navbar.Link>
-        <Navbar.Link href="#" className="text-xl text-white">Services</Navbar.Link>
-        <Navbar.Link href="#" className="text-xl text-white">Pricing</Navbar.Link>
-        <Navbar.Link href="#" className="text-xl text-white">Contact</Navbar.Link>
-        <Navbar.Link href="/Userlogin" className="text-xl text-white ">Login</Navbar.Link>
-      
-        
-        <Navbar.Link href="#" className="text-xl  text-red-700">Logout</Navbar.Link>
-        <img src={logout} alt="logout img" className="border-1 hover:bg-s mt-[10px] h-8 md:inline-flex  mb-[10px]"/> 
-
+        <Navbar.Link href="#" active className="lg:text-xl"> Home </Navbar.Link>
+        <Navbar.Link href="#" className="lg:text-xl">About</Navbar.Link>
+        <Navbar.Link href="#" className="lg:text-xl">Services</Navbar.Link>
+        <Navbar.Link href="#" className="lg:text-xl">Pricing</Navbar.Link>
+        <Navbar.Link href="#" className="lg:text-xl">Contact</Navbar.Link>
+        <div className="flex md:order-2">
+          <Button className=" mb-[5px] flex items-center gap-1">
+            <p className=" me-[5px] text-red-600 lg:text-xl">Logout</p>
+            <img src={logout} alt="logout btn" className="h-6" />
+          </Button>
+        </div>
       </Navbar.Collapse>
     </Navbar>
   );
--- client/src/views/App.jsx
+++ client/src/views/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Nav } from '../components/Navbar/navbar.jsx'
+import { Slider } from '../components/Carousel/carousel.jsx'
 function App() {
     return (
         <div>
         <Nav/>
+        <Slider/>
 
         </div>
     )
