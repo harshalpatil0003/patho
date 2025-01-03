417fa20c4746364406c196e075b4d337aed9b1f0
Phobial <harshalpatilcsn2020@gmail.com>
2025-01-04 00:02:44 +0530

    footer is edited

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,51 +3,50 @@
 
 import { Footer } from "flowbite-react";
 import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
-import logo from '../../Assets/navbar/logo.png'
 
 export function Foot() {
   return (
-    <Footer container className="">
+    <Footer bgDark >
       <div className="w-full">
-        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+        <div className="text-white grid w-full grid-cols-2 gap-8 px-6 py-8 md:grid-cols-4">
           <div>
-            <Footer.Brand
-              href="/"
-              src={logo}
-              alt="Flowbite Logo"
-            />
+            <Footer.Title title="Company"  className="text-white text-2xl"/>
+            <Footer.LinkGroup col  className="text-white text-xl">
+              <Footer.Link href="#" >About</Footer.Link>
+              <Footer.Link href="#">Careers</Footer.Link>
+              <Footer.Link href="#">Brand Center</Footer.Link>
+              <Footer.Link href="#">Blog</Footer.Link>
+            </Footer.LinkGroup>
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-            <div>
-              <Footer.Title title="about" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Home</Footer.Link>
-                <Footer.Link href="#">About</Footer.Link>
-                <Footer.Link href="#">Services</Footer.Link>
-                <Footer.Link href="#">Pricing</Footer.Link>
-                <Footer.Link href="#">Contact</Footer.Link>
-
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Follow us" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Github</Footer.Link>
-                <Footer.Link href="#">Discord</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Legal" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
+          <div>
+            <Footer.Title title="help center" className="text-white text-2xl" />
+            <Footer.LinkGroup col  className="text-white text-xl">
+              <Footer.Link href="#">Discord Server</Footer.Link>
+              <Footer.Link href="#">Twitter</Footer.Link>
+              <Footer.Link href="#">Facebook</Footer.Link>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="legal" className="text-white text-2xl"/>
+            <Footer.LinkGroup col  className="text-white text-xl">
+              <Footer.Link href="#">Privacy Policy</Footer.Link>
+              <Footer.Link href="#">Licensing</Footer.Link>
+              <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="download" className="text-white text-2xl"/>
+            <Footer.LinkGroup col  className="text-white text-xl">
+              <Footer.Link href="#">iOS</Footer.Link>
+              <Footer.Link href="#">Android</Footer.Link>
+              <Footer.Link href="#">Windows</Footer.Link>
+              <Footer.Link href="#">MacOS</Footer.Link>
+            </Footer.LinkGroup>
           </div>
         </div>
-        <Footer.Divider />
-        <div className="w-full sm:flex sm:items-center sm:justify-between">
-          <Footer.Copyright href="#" by="Flowbite™" year={2022} />
+        <div className="w-full bg-gray-700 px-4 py-6 sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright href="#" by="GoLab™" year={2025} className="text-white text-xl"/>
           <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
