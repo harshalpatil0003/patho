0c741924b92d5d6dea3cf3efd453d614f57bad6f
Phobial <harshalpatilcsn2020@gmail.com>
2025-01-10 11:49:13 +0530

    changed color of signin and signup button

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -89,7 +89,7 @@ export default function UserLogin() {
                                 <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
                                     <button
                                         type="submit" // Change to type submit
-                                        className="w-[100%] inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-xl font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 md:w-[400px]">
+                                        className="w-[100%] inline-block shrink-0 rounded-md border border-cyan-600 bg-cyan-800 px-12 py-3 text-xl font-medium text-white transition hover:bg-transparent hover:text-cyan-600 focus:outline-none focus:ring active:text-cyan-500 md:w-[400px]">
                                         Login
                                     </button>
                                 </div>
