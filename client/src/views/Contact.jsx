fecab51645fe29642c077f9fa02ec02810eefeff
Phobial <harshalpatilcsn2020@gmail.com>
2025-01-19 01:06:15 +0530

    card for contact is added

--- client/src/views/Contact.jsx
+++ client/src/views/Contact.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Card, Button } from 'flowbite-react'
+
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -63,13 +65,29 @@ const ContactForm = () => {
 
   return (
     <div className="contact-form">
-      <h1 id='Contact' className='m-3 text-center text-3xl font-bold lg:text-5xl mt-10'>Contact</h1>
-      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-        <section className="relative flex h-32  lg:col-span-5 lg:h-full xl:col-span-6">
-          <p>Need assistance or have a question? We're here to help! Whether you’re looking to book a test, understand your results, or learn more about our services, feel free to reach out. Your health is our priority.</p>
+      <h1 id='Contact' className='m-3 text-center text-3xl font-bold lg:text-5xl'>Contact</h1>
+      <div className="lg:grid  lg:grid-cols-12">
+        <section className="p-7 mb-20 h-32  lg:col-span-5 lg:h-full xl:col-span-6">
+
+          <p className="text-justify  lg:text-xl  " >&emsp; Need assistance or have a question? We're here to help! Whether you’re looking to book a test, understand your results, or learn more about our services, feel free to reach out. Your health is our priority.</p>
+          <p className="text-justify  lg:text-xl "> &emsp;Fill out the form beside, and our team will get back to you as soon as possible. For urgent inquiries, please use the contact details provided.</p>
+
+          <Card className="max-w-sm mt-6">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+       Connect with us
+      </h5>
+      <p className="font-semibold text-gray-700 dark:text-gray-400">
+      Speak to a Representative
+      <Button gradientMonochrome="teal" className="font-bold" href="tel:8767887013">Call</Button>
+      </p>
+      <p className="font-semibold text-gray-700 dark:text-gray-400">Reach Out via Email
+      <Button gradientMonochrome="teal" className="" href="mailto:harshalpatilcsn2020@gmail.com">Email</Button>
+      </p>
+    </Card>
+
         </section>
         <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-2 xl:col-span-6 border border-red-600">
-          <div className="max-w-xl lg:max-w-3xl mx-auto">
+          <div className="max-w-xl mt-10 lg:max-w-3xl mx-auto">
             <form onSubmit={handleSubmit} encType="multipart/form-data" className=" grid grid-cols-6 gap-6 ">
               <div className="col-span-6">
                 <label htmlFor="name" className="block text-xl font-medium text-gray-700">Full Name</label>
@@ -143,7 +161,7 @@ const ContactForm = () => {
         </main>
 
       </div>
-    </div>
+    </div >
 
   );
 };
